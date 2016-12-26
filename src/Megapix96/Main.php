dfacb62f7a780e589efb733fddf86c8c7b2099b8
Megapix96 <official.megapixel@gmail.com>
2016-12-26 20:03:09 +0900

    Fix #1

--- src/Megapix96/Main.php
+++ src/Megapix96/Main.php
@@ -1,12 +1,14 @@
 <?php
 namespace Megapix96;
 
+use pocketmine\Player;
 use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\network\protocol\AddEntityPacket;
 use pocketmine\network\protocol\MoveEntityPacket;
+use pocketmine\network\protocol\RemoveEntityPacket;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener{
@@ -38,27 +40,12 @@ class Main extends PluginBase implements Listener{
 			Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, $n],
 			Entity::DATA_FLAGS => [Entity::DATA_TYPE_LONG, $flags]
 		];
-        	$this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
-        	foreach ($this->getServer()->getOnlinePlayers() as $pl){
+
+		foreach ($p->getLevel()->getPlayers() as $pl){
         		if ($pl->getName() !== $n){
-	        		$pk = new AddEntityPacket();
-	        		$pk->eid = $this->eid[$pl->getName()];
-	        		$pk->type = self::NETWORK_ID;
-	        		$pk->x = $p->x;
-	        		$pk->y = $p->y;
-	        		$pk->z = $p->z;
-	        		$flags = 0;
-		          	$flags |= 1 << Entity::DATA_FLAG_CAN_SHOW_NAMETAG;
-		          	$flags |= 1 << Entity::DATA_FLAG_ALWAYS_SHOW_NAMETAG;
-		          	$flags |= 1 << Entity::DATA_FLAG_IMMOBILE;
-		        	$pk->metadata = [
-					Entity::DATA_LEAD_HOLDER => [Entity::DATA_TYPE_SHORT, -1],
-					Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, $n],
-					Entity::DATA_FLAGS => [Entity::DATA_TYPE_LONG, $flags]
-				];
-				$p->dataPacket($pk);
+				$pl->dataPacket($pk);
 			}
-        	}
+		}
         }
 
         function onMove(PlayerMoveEvent $ev){
@@ -71,6 +58,42 @@ class Main extends PluginBase implements Listener{
         	$pk->z = $p->z;
         	$pk->yaw = $p->yaw;
         	$pk->pitch = $p->pitch;
-        	$this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
+        	foreach ($p->getLevel()->getPlayers() as $pl){
+        		$pl->dataPacket($pk);
+        	}
         }
+
+	function onLevelChange(EntityLevelChangeEvent $ev){
+		$p = $ev->getEntity();
+		$n = $p->getName();
+		if ($p instanceof Player){
+			foreach ($ev->getOrigin()->getPlayers() as $pl){
+				if ($pl->getName() !== $n){
+					$pk = new RemoveEntityPacket();
+					$pk->eid = $this->eid[$n];
+					$pl->dataPacket($pk);
+				}
+			}
+			foreach ($ev->getTarget()->getPlayers() as $pl){
+				if ($pl->getName() !== $n){
+					$pk = new AddEntityPacket();
+			        	$pk->eid = $this->eid[$n];
+			        	$pk->type = self::NETWORK_ID;
+			        	$pk->x = $p->x;
+			        	$pk->y = $p->y;
+			        	$pk->z = $p->z;
+			        	$flags = 0;
+			          	$flags |= 1 << Entity::DATA_FLAG_CAN_SHOW_NAMETAG;
+			          	$flags |= 1 << Entity::DATA_FLAG_ALWAYS_SHOW_NAMETAG;
+			          	$flags |= 1 << Entity::DATA_FLAG_IMMOBILE;
+			        	$pk->metadata = [
+						Entity::DATA_LEAD_HOLDER => [Entity::DATA_TYPE_SHORT, -1],
+						Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, $n],
+						Entity::DATA_FLAGS => [Entity::DATA_TYPE_LONG, $flags]
+					];
+					$pl->dataPacket($pk);
+				}
+			}
+		}
+	}
 }
